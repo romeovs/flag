42097cd027b7eaadf0fbedc8625557ff226af913
Romeo Van Snick <romeovs@gmail.com>
2015-06-24 00:52:13 +0200

    extend usability

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import yargs from 'yargs';
 
-export default function(name) {
-  return process.env[name] === '1' || yargs.argv[name] || false;
-}
+const one = function (name) {
+  const caps = name.toUpperCase();
+  return yargs.argv[name]
+    || process.env[name] === '1'
+    || process.env[caps] === '1'
+    || process.env[name] === 'true'
+    || process.env[caps] === 'true'
+    || false;
+};
+
+export default function (names) {
+  return names
+    .map(one)
+    .reduce((a, b) => a || b, false);
+};
+
